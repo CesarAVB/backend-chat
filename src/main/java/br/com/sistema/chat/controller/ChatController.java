8fdf4c144f3f916f8bd8c7b70705d56f378c5cc6
César Augusto <gutobrrj1@hotmail.com>
2025-12-17 22:47:26 -0300

    first commit

---
+++ src/main/java/br/com/sistema/chat/controller/ChatController.java
@@ -0,0 +1,20 @@
+package br.com.sistema.chat.controller;
+
+import java.time.LocalDateTime;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import br.com.sistema.chat.model.ChatMessage;
+
+@Controller
+public class ChatController {
+
+	@MessageMapping("/chat.send/{room}")
+	@SendTo("/topic/chat/{room}")
+	public ChatMessage sendToRoom(@DestinationVariable String room, ChatMessage message) {
+		message.setTimestamp(LocalDateTime.now());
+		return message;
+	}
+	
+}
---
+++ src/test/java/br/com/sistema/chat/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.sistema.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
